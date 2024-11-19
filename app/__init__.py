83a528c1e4830f65b1a2b64c2234e2b2c6105464
Ingrid Dahiana Enriquez Dos Santos <ingridenriquez74@gmail.com>
2024-11-19 15:23:20 -0300

    add:cambios

---
+++ app/__init__.py
@@ -0,0 +1,98 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# importar referenciales
+from app.rutas.referenciales.ciudad.ciudad_routes import ciumod 
+from app.rutas.referenciales.persona.persona_routes import persona_mod
+from app.rutas.referenciales.medico.medico_routes import medicomod
+from app.rutas.referenciales.paciente.paciente_routes import pacientemod
+from app.rutas.referenciales.ocupacion.ocupacion_routes import ocumod
+from app.rutas.referenciales.turno.turno_routes import turmod
+from app.rutas.referenciales.estado_civil.estado_civil_routes import estmod
+from app.rutas.referenciales.enfermedad.enfermedad_routes import enfmod
+from app.rutas.referenciales.genero.genero_routes import genmod
+from app.rutas.referenciales.dia.dia_routes import diamod
+from app.rutas.referenciales.hora.hora_routes import hormod
+
+# registrar referenciales
+modulo0 = '/referenciales'
+app.register_blueprint(ciumod, url_prefix=f'{modulo0}/ciudad')
+
+from app.rutas.referenciales.ciudad.ciudad_api import ciuapi
+
+
+modulo0 = '/referenciales'
+app.register_blueprint(persona_mod, url_prefix=f'{modulo0}/persona')
+
+from app.rutas.referenciales.persona.persona_api import personaapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(medicomod, url_prefix=f'{modulo0}/medico')
+
+from app.rutas.referenciales.medico.medico_api import medicoapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(pacientemod, url_prefix=f'{modulo0}/paciente')
+
+from app.rutas.referenciales.paciente.paciente_api import pacienteapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(ocumod, url_prefix=f'{modulo0}/ocupacion')
+
+from app.rutas.referenciales.ocupacion.ocupacion_api import ocuapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(turmod, url_prefix=f'{modulo0}/turno')
+
+from app.rutas.referenciales.turno.turno_api import turapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(estmod, url_prefix=f'{modulo0}/estado_civil')
+
+from app.rutas.referenciales.estado_civil.estado_civil_api import estapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(enfmod, url_prefix=f'{modulo0}/enfermedad')
+
+from app.rutas.referenciales.enfermedad.enfermedad_api import enfapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(genmod, url_prefix=f'{modulo0}/genero')
+
+from app.rutas.referenciales.genero.genero_api import genapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(diamod, url_prefix=f'{modulo0}/dia')
+
+from app.rutas.referenciales.dia.dia_api import diaapi
+
+modulo0 = '/referenciales'
+app.register_blueprint(hormod, url_prefix=f'{modulo0}/hora')
+
+from app.rutas.referenciales.hora.hora_api import horapi
+
+
+# APIS v1
+version1 = '/api/v1'
+app.register_blueprint(ciuapi, url_prefix=version1)
+
+app.register_blueprint(personaapi, url_prefix=version1)
+
+app.register_blueprint(medicoapi,url_prefix=version1)
+
+app.register_blueprint(pacienteapi, url_prefix=version1)
+
+app.register_blueprint(ocuapi, url_prefix=version1)
+
+app.register_blueprint(turapi, url_prefix=version1)
+
+app.register_blueprint(estapi, url_prefix=version1)
+
+app.register_blueprint(enfapi, url_prefix=version1)
+
+app.register_blueprint(genapi, url_prefix=version1)
+
+app.register_blueprint(diaapi, url_prefix=version1)
+
+app.register_blueprint(horapi, url_prefix=version1)
--- app/rutas/referenciales/ciudad/ciudad_api.py
+++ app/rutas/referenciales/ciudad/ciudad_api.py
@@ -7,16 +7,13 @@ ciuapi = Blueprint('ciuapi', __name__)
 @ciuapi.route('/ciudades', methods=['GET'])
 def getCiudades():
     ciudao = CiudadDao()
-
     try:
         ciudades = ciudao.getCiudades()
-
         return jsonify({
             'success': True,
             'data': ciudades,
             'error': None
         }), 200
-
     except Exception as e:
         app.logger.error(f"Error al obtener todas las ciudades: {str(e)}")
         return jsonify({
@@ -24,13 +21,12 @@ def getCiudades():
             'error': 'Ocurrió un error interno. Consulte con el administrador.'
         }), 500
 
-@ciuapi.route('/ciudades/<int:ciudad_id>', methods=['GET'])
-def getCiudad(ciudad_id):
+# Obtiene una ciudad por ID
+@ciuapi.route('/ciudades/<int:id_ciudad>', methods=['GET'])
+def getCiudad(id_ciudad):
     ciudao = CiudadDao()
-
     try:
-        ciudad = ciudao.getCiudadById(ciudad_id)
-
+        ciudad = ciudao.getCiudadById(id_ciudad)
         if ciudad:
             return jsonify({
                 'success': True,
@@ -42,7 +38,6 @@ def getCiudad(ciudad_id):
                 'success': False,
                 'error': 'No se encontró la ciudad con el ID proporcionado.'
             }), 404
-
     except Exception as e:
         app.logger.error(f"Error al obtener ciudad: {str(e)}")
         return jsonify({
@@ -55,29 +50,30 @@ def getCiudad(ciudad_id):
 def addCiudad():
     data = request.get_json()
     ciudao = CiudadDao()
-
-    # Validar que el JSON no esté vacío y tenga las propiedades necesarias
     campos_requeridos = ['descripcion']
 
-    # Verificar si faltan campos o son vacíos
+    # Validación de campos requeridos
     for campo in campos_requeridos:
-        if campo not in data or data[campo] is None or len(data[campo].strip()) == 0:
+        if campo not in data or not data[campo].strip():
             return jsonify({
-                            'success': False,
-                            'error': f'El campo {campo} es obligatorio y no puede estar vacío.'
-                            }), 400
+                'success': False,
+                'error': f'El campo {campo} es obligatorio y no puede estar vacío.'
+            }), 400
 
     try:
-        descripcion = data['descripcion'].upper()
-        ciudad_id = ciudao.guardarCiudad(descripcion)
-        if ciudad_id is not None:
+        descripcion = data['descripcion'].strip().upper()
+        id_ciudad = ciudao.guardarCiudad(descripcion)
+        if id_ciudad:
             return jsonify({
                 'success': True,
-                'data': {'id': ciudad_id, 'descripcion': descripcion},
+                'data': {'id_ciudad': id_ciudad, 'descripcion': descripcion},
                 'error': None
             }), 201
         else:
-            return jsonify({ 'success': False, 'error': 'No se pudo guardar la ciudad. Consulte con el administrador.' }), 500
+            return jsonify({
+                'success': False,
+                'error': 'No se pudo guardar la ciudad. Consulte con el administrador.'
+            }), 500
     except Exception as e:
         app.logger.error(f"Error al agregar ciudad: {str(e)}")
         return jsonify({
@@ -85,27 +81,27 @@ def addCiudad():
             'error': 'Ocurrió un error interno. Consulte con el administrador.'
         }), 500
 
-@ciuapi.route('/ciudades/<int:ciudad_id>', methods=['PUT'])
-def updateCiudad(ciudad_id):
+# Actualiza una ciudad
+@ciuapi.route('/ciudades/<int:id_ciudad>', methods=['PUT'])
+def updateCiudad(id_ciudad):
     data = request.get_json()
     ciudao = CiudadDao()
-
-    # Validar que el JSON no esté vacío y tenga las propiedades necesarias
     campos_requeridos = ['descripcion']
 
-    # Verificar si faltan campos o son vacíos
+    # Validación de campos requeridos
     for campo in campos_requeridos:
-        if campo not in data or data[campo] is None or len(data[campo].strip()) == 0:
+        if campo not in data or not data[campo].strip():
             return jsonify({
-                            'success': False,
-                            'error': f'El campo {campo} es obligatorio y no puede estar vacío.'
-                            }), 400
-    descripcion = data['descripcion']
+                'success': False,
+                'error': f'El campo {campo} es obligatorio y no puede estar vacío.'
+            }), 400
+
     try:
-        if ciudao.updateCiudad(ciudad_id, descripcion.upper()):
+        descripcion = data['descripcion'].strip().upper()
+        if ciudao.updateCiudad(id_ciudad, descripcion):
             return jsonify({
                 'success': True,
-                'data': {'id': ciudad_id, 'descripcion': descripcion},
+                'data': {'id_ciudad': id_ciudad, 'descripcion': descripcion},
                 'error': None
             }), 200
         else:
@@ -120,16 +116,15 @@ def updateCiudad(ciudad_id):
             'error': 'Ocurrió un error interno. Consulte con el administrador.'
         }), 500
 
-@ciuapi.route('/ciudades/<int:ciudad_id>', methods=['DELETE'])
-def deleteCiudad(ciudad_id):
+# Elimina una ciudad
+@ciuapi.route('/ciudades/<int:id_ciudad>', methods=['DELETE'])
+def deleteCiudad(id_ciudad):
     ciudao = CiudadDao()
-
     try:
-        # Usar el retorno de eliminarCiudad para determinar el éxito
-        if ciudao.deleteCiudad(ciudad_id):
+        if ciudao.deleteCiudad(id_ciudad):
             return jsonify({
                 'success': True,
-                'mensaje': f'ciudad con ID {ciudad_id} eliminada correctamente.',
+                'mensaje': f'Ciudad con ID {id_ciudad} eliminada correctamente.',
                 'error': None
             }), 200
         else:
@@ -137,7 +132,6 @@ def deleteCiudad(ciudad_id):
                 'success': False,
                 'error': 'No se encontró la ciudad con el ID proporcionado o no se pudo eliminar.'
             }), 404
-
     except Exception as e:
         app.logger.error(f"Error al eliminar ciudad: {str(e)}")
         return jsonify({
